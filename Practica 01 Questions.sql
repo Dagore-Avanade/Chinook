5ea47ceb2e225c06305e4359278c8a3a4638be8e
David Gómez Redondo <david.gomez.redondo@avanade.com>
2023-04-17 15:10:56 +0200

    first commit

---
+++ Practica 01 Questions.sql
@@ -0,0 +1,49 @@
+---------------------------------------------------------------------------------------------------------
+-- ejercicios iniciales 1
+---------------------------------------------------------------------------------------------------------
+
+
+USE Chinook
+GO
+
+-- Obtener el nombre, apellidos, ciudad y país de cada cliente.
+
+
+-- Obtener todos los artistas cuyo nombre comiencen por "G" (No case sensitive).
+
+
+
+-- Obtener todos los artistas que contengan "GU" en su nombre (no case sensitive).
+
+
+-- Obtener todos los artistas y sus correspondientes álbumes (tengan o no tengan).
+
+
+-- Obtener todos los artistas que no tengan álbumes.
+
+
+-- Obtener todos los artistas con el número total de álbumes que tiene (tengan o no).
+
+
+-- Obtener todos los artistas que tengan 3 o más álbumes 
+
+
+-- Obtener todos los artistas con el número total de álbumes que tiene (que tengan).
+
+
+-- Obtener todos los artistas con más de 3 álbumes y ordenados por el número de álbumes de menor a mayor.
+
+
+-- Obtener todos los medios digitales existentes con el número de canciones existentes por cada uno de ellos.
+
+
+-- Obtener todas las canciones de género Rock.
+
+
+
+
+
+
+ 
+
+
